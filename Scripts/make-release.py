e4c52f789cd7378663b9819fa79cdfd1754bdb5a
Keeper <hd_keeper@mail.ru>
2023-05-22 00:02:42 +0300

    Описание скриптов

--- Scripts/make-release.py
+++ Scripts/make-release.py
@@ -1,4 +1,4 @@
-# Создание zip-архива для архива Kage
+# Создание ZIP-архива с субтитрами для архива Kage
 
 from os import listdir
 from os.path import isfile, join, realpath, dirname
@@ -8,8 +8,12 @@ from zipfile import ZipFile, ZIP_DEFLATED, ZIP_LZMA
 root_dir = realpath(join(dirname(__file__), '..'))
 sub_dir = join(root_dir, '2-Russian')
 
+def get_ep(file_name: str):
+    m = re.search(r'(\d+)', file_name)
+    return None if m is None else int(m[1])
+
 file_names = [f for f in listdir(sub_dir) if isfile(join(sub_dir, f))]
-episodes = list(map(lambda s: int(re.search(r'(\d+)', s)[1]), file_names))
+episodes = [get_ep(f) for f in file_names if get_ep(f) is not None]
 zip_file_name = 'Candy-Candy TV %d-%d.zip' % (min(episodes), max(episodes))
 
 with ZipFile(zip_file_name, 'w', compression=ZIP_LZMA, compresslevel=9) as zf:
