907e02debb612100198e14e377f4f3f503483427
Keeper <hd_keeper@mail.ru>
2023-05-26 14:27:02 +0300

    Update make-release.py

--- Scripts/make-release.py
+++ Scripts/make-release.py
@@ -12,9 +12,28 @@ def get_ep(file_name: str):
     m = re.search(r'(\d+)', file_name)
     return None if m is None else int(m[1])
 
+def zip_eps(episodes: list[int]) -> str:
+    ints: list[list[int]] = []
+    for n in episodes:
+        if len(ints) == 0:
+            ints.append([n])
+            continue
+
+        last = ints[-1]
+        if last[-1] == n-1:
+            if len(last) == 1:
+                last.append(n)
+            else:
+                last[1] = n
+        else:
+            ints.append([n])
+
+    return ','.join('-'.join(str(n) for n in i) for i in ints)
+
+
 file_names = [f for f in listdir(sub_dir) if isfile(join(sub_dir, f))]
-episodes = [get_ep(f) for f in file_names if get_ep(f) is not None]
-zip_file_name = 'Candy-Candy TV %d-%d.zip' % (min(episodes), max(episodes))
+episodes = sorted([get_ep(f) for f in file_names if get_ep(f) is not None])
+zip_file_name = 'Candy-Candy TV %s.zip' % zip_eps(episodes)
 
 with ZipFile(zip_file_name, 'w', compression=ZIP_LZMA, compresslevel=9) as zf:
     zf.write(join(root_dir, 'README.txt'), 'README.txt')
